47e41aaa160e1c7e36e9f63840a8e246fca75587
Neladnov Dmitry <gasdom@yandex.ru>
2019-01-10 11:16:20 +0300

    ver.1.0

--- hangman_main.rb
+++ hangman_main.rb
@@ -6,10 +6,13 @@ puts "Игра виселица. Версия 1.\n\n"
 sleep 1
 
 printer = ResultPrinter.new
-reader = WordReader.new::SystemCallError puts 'Файл со словами не найден!'
+reader = WordReader.new
+# begin
+#     SystemCallError puts 'Файл со словами не найден!'
+# end
 
 current_path = File.dirname(__FILE__)
-word = reader.read_from_file(current_path + '/data/words.txt')
+word = reader.read_from_file(current_path + 'data/words.txt')
 
 game = Game.new(word)
 
